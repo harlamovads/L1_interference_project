1db3d051b870ac790ffa357c8cc505ff24731e96
Zloveschee_Oblachko <90269992+harlamovads@users.noreply.github.com>
2023-05-24 22:59:30 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import spacy
 from spacy import displacy
 
 text = '' #в переменную text надо передать текст для разбора
-nlp1 = spacy.load('path-to-model') #надо вставить путь к скачанной с Google Disk папке
+nlp1 = spacy.load('path-to-model') #надо вставить путь к одной из двух моделей в скачанной с Google Disk папке, например, /content/drive/MyDrive/general_model/model-best
 doc = nlp1(text)
 displacy.render(doc, style='span', jupyter=True)
